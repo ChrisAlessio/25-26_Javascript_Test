9680d9b8dbee3b9ed4a15495d7f8292e4b78ce37
13923 <Christopher.alessio@vtsd.com>
2025-12-15 08:41:02 -0500

    12/15/25 Javascript day 10 Java Test

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,9 @@
 /* Put code for the task below */
+function showButton(){
+    document.getElementById("buttonTask1").innerHTML="<button onclick = 'hideImage()' style='display:' id='buttonTask1'>Click Me</button>"
+    //alert("test")
+}
+
+function hideImage(){
+    document.getElementById("imageTask1").style.display="none"
+}
--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,14 +1,29 @@
 /* Put code for the task below */
 function nameEmpty(){
-    if(document.getElementById("fullName").)
-
+    if(document.getElementById("fullName").value === ""){
+        document.getElementById("fullName").style.backgroundColor="red"
+    }else{
+        document.getElementById("fullName").style.backgroundColor="white"
+    }
 }
 function addressEmpty(){
-
+    if(document.getElementById("address").value === ""){
+        document.getElementById("address").style.backgroundColor="red"
+    }else{
+        document.getElementById("address").style.backgroundColor="white"
+    }
 }
 function phoneEmpty(){
-
+    if(document.getElementById("phone").value === ""){
+        document.getElementById("phone").style.backgroundColor="red"
+    }else{
+        document.getElementById("phone").style.backgroundColor="white"
+    }
 }
 function emailEmpty(){
-
+    if(document.getElementById("email").value === ""){
+        document.getElementById("email").style.backgroundColor="red"
+    }else{
+        document.getElementById("email").style.backgroundColor="white"
+    }
 }
